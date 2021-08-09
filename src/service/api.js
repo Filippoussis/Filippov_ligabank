0a4892087cc3e412e3f666ded7c940016f97697e
Andrey Filippov <androsphilippos@gmail.com>
2021-08-09 11:31:06 +0500

    создал апи клиента -> подключаюсь к Франкфурскому апи-банка

---
+++ src/service/api.js
@@ -0,0 +1,17 @@
+export default class Api {
+
+  _host = 'https://api.frankfurter.app';
+  _currencies = 'RUB,EUR,GBP,CNY';
+
+  async getCurrencyRates(date) {
+    const res = await fetch(`${this._host}/${date}?from=USD&to=${this._currencies}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${`${this._host}/${date}?from=USD&to=${this._currencies}`}, received ${res.status}`);
+    }
+
+    const body = await res.json();
+
+    return body;
+  };
+}
