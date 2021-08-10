1f066cfb0896ba42820ec8c07470147159e91daf
Andrey Filippov <androsphilippos@gmail.com>
2021-08-10 17:51:41 +0500

    добавил компонент weekend-message

---
+++ src/components/weekend-message/weekend-message.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './weekend-message.scss';
+
+function WeekendMessage() {
+  return <p className="weekend-message"><sup>*</sup>&nbsp;в выходные дни котировки предоставляются на пятницу</p>
+}
+
+export default WeekendMessage;
