de843628011e8c6dc00b18ffb7c229cc2ed55d24
Andrey Filippov <androsphilippos@gmail.com>
2021-08-10 17:52:44 +0500

    вынес в const: CURRENCIES, BASE_CURRENCY, DefaultFormData, DEFAULT_DATE_REQUEST

--- src/const.js
+++ src/const.js
@@ -12,3 +12,15 @@ export const SITE_MENU_LIST_FOOTER = [
   'Контакты',
   'Задать вопрос',
 ];
+
+export const CURRENCIES = ['RUB', 'USD', 'EUR', 'GBP', 'CNY'];
+export const BASE_CURRENCY = 'USD';
+
+export const DefaultFormData = {
+  TYPE_CURRENCY_SELL: 'RUB',
+  TYPE_CURRENCY_BUY: 'USD',
+  AMOUNT_CURRENCY_SELL: '1000',
+  AMOUNT_CURRENCY_BUY: '',
+};
+
+export const DEFAULT_DATE_REQUEST = 'latest';
