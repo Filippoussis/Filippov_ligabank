905dff6d6547747af0dcf21e72189143f0af2b7c
Andrey Filippov <androsphilippos@gmail.com>
2021-08-10 17:53:12 +0500

    небоьшой рефакт api

--- src/service/api.js
+++ src/service/api.js
@@ -1,13 +1,15 @@
+import {BASE_CURRENCY} from '../const';
+
 export default class Api {
 
   _host = 'https://api.frankfurter.app';
   _currencies = 'RUB,EUR,GBP,CNY';
 
   async getCurrencyRates(date) {
-    const res = await fetch(`${this._host}/${date}?from=USD&to=${this._currencies}`);
+    const res = await fetch(`${this._host}/${date}?from=${BASE_CURRENCY}&to=${this._currencies}`);
 
     if (!res.ok) {
-      throw new Error(`Could not fetch ${`${this._host}/${date}?from=USD&to=${this._currencies}`}, received ${res.status}`);
+      throw new Error(`Could not fetch ${`${this._host}/${date}?from=${BASE_CURRENCY}&to=${this._currencies}`}, received ${res.status}`);
     }
 
     const body = await res.json();
